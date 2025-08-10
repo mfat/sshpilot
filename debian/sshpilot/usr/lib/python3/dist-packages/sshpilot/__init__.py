b43fc067f12e23dff0a2b1f665c854ac2421eeaa
Mehdi <mah.fat@gmail.com>
2025-08-10 23:33:08 +0330

    added debian

---
+++ debian/sshpilot/usr/lib/python3/dist-packages/sshpilot/__init__.py
@@ -0,0 +1,7 @@
+# sshPilot - SSH connection manager with integrated terminal
+# Copyright (C) 2025 mFat <newmfat@gmail.com>
+# Licensed under GPL-3.0
+
+__version__ = "2.1.0"
+__author__ = "mFat"
+__email__ = "newmfat@gmail.com"
