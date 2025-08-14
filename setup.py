7e681e3cc522dfc0e03c2f2089fe1adefd80b7c1
Mehdi <mah.fat@gmail.com>
2025-08-14 16:00:55 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -15,6 +15,17 @@ OPTIONS = {
         '/opt/homebrew/lib/girepository-1.0',
         '/opt/homebrew/share/adwaita-icon-theme'
     ],
+    'frameworks': [
+        '/opt/homebrew/opt/gtk4/lib/libgtk-4.1.dylib',
+        '/opt/homebrew/opt/glib/lib/libglib-2.0.0.dylib',
+        '/opt/homebrew/opt/vte3/lib/libvte-2.91-gtk4.0.dylib',
+        '/opt/homebrew/opt/icu4c/lib/libicudata.75.dylib',
+        '/opt/homebrew/opt/icu4c/lib/libicui18n.75.dylib',
+        '/opt/homebrew/opt/icu4c/lib/libicuio.75.dylib',
+        '/opt/homebrew/opt/icu4c/lib/libicutu.75.dylib',
+        '/opt/homebrew/opt/icu4c/lib/libicuuc.75.dylib',
+        '/opt/homebrew/opt/graphene/lib/libgraphene-1.0.0.dylib'
+    ],
     'plist': {
         'CFBundleDevelopmentRegion': 'English',
         'CFBundleIdentifier': 'com.sshpilot.app',
