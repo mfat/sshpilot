3d71ca3236ce2baa4fb96d705174e5779dd1e261
Mehdi <mah.fat@gmail.com>
2025-09-10 23:51:07 +0330

    Prevent welcome page from blocking quick connect focus

--- sshpilot/welcome_page.py
+++ sshpilot/welcome_page.py
@@ -28,10 +28,7 @@ class WelcomePage(Gtk.Box):
         self.set_margin_end(24)
         self.set_margin_top(24)
         self.set_margin_bottom(24)
-
-
-
-       
+        self.set_focus_on_click(False)
 
         # Quick connect box
         self.quick_box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=6)
