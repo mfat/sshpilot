5b4d1a52cd87a6496bfc3bcda4bc5fa9c295329a
Kainoa Kanter <kainoa@t1c.dev>
2025-10-11 11:51:14 -0700

    feat: add more common terminals
    - Ghostty (added for Linux, was added for macOS already) - Foot -
    Blackbox

--- sshpilot/preferences.py
+++ sshpilot/preferences.py
@@ -3081,6 +3081,9 @@ class PreferencesWindow(Gtk.Window):
                 ("tilix", "tilix"),
                 ("xterm", "xterm"),
                 ("guake", "guake"),
+                ("ghostty", "ghostty"),
+                ("foot", "foot"),
+                ("blackbox", "blackbox"),
             ]
 
             # Append macOS terminals when running on macOS
